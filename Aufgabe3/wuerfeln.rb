eeae72752cc2697f280ea48fef39629043629a49
kontez <kontez@users.noreply.github.com>
2016-04-14 21:23:16 +0200

    added test output

--- Aufgabe3/wuerfeln.rb
+++ Aufgabe3/wuerfeln.rb
@@ -41,3 +41,13 @@ def the_winner_takes_it_all(zahl,folge)
   end
 end
 
+
+
+#tests
+puts "[+] Generating the winning six Lotto numbers..."
+#Okay I realized that this was a bad idea and that I need to call the method 6 times :/
+#puts (lotto_zahl().to_s+' ')*6
+puts lotto_zahl().to_s+' '+lotto_zahl().to_s+' '+lotto_zahl().to_s+' '+lotto_zahl().to_s+' '+lotto_zahl().to_s+' '+lotto_zahl().to_s
+puts "[+] "+anzahl_wuerfe_fuer(3).to_s+" tries needed to roll the 3."
+puts "[+] "+anzahl_versuche_fuer_zahl_in_folge(4,3).to_s+" tries needed to roll the 4 three times in sequence."
+puts "[+] Player number "+the_winner_takes_it_all(2,4).to_s+" is the winner since he first rolled 4 times the 2."
