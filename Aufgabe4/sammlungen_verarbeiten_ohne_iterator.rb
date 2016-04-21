ad952286b05ea8b8583876ad7b959c48b48f44ec
kontez <kontez@users.noreply.github.com>
2016-04-21 09:49:46 +0200

    new excercise

---
+++ Aufgabe4/sammlungen_verarbeiten_ohne_iterator.rb
@@ -0,0 +1,58 @@
+# 1.1
+def ein_dim_ary_elemente_ausgeben(ary)
+
+end
+
+# 1.2
+def ein_dim_ary_kopieren(ary)
+
+end
+
+# 1.3
+def ein_dim_ary_teiler_von_3(n)
+
+end
+
+# 1.4
+def ein_dim_ary_max(ary)
+
+end
+
+# 1.5
+def ein_dim_ary_sum(ary)
+
+end
+
+# 1.6
+def zwei_dim_ary_ausgeben(ary)
+
+end
+
+# 1.7
+
+def zwei_dim_ary_kopieren(ary)
+
+end
+
+# 1.8
+
+def drei_dim_ary_kopieren(ary)
+end
+
+# 1.9
+
+def zwei_dim_ary_sum(ary)
+
+end
+
+# 1.10
+def drei_dim_ary_sum(ary)
+
+end
+
+
+# 1.11
+
+def zwei_dim_ary_transponiere(ary)
+
+end
