b5c8c9d104390559c7a88fb505f11d664a0f3098
kontez <kontez@users.noreply.github.com>
2016-05-13 09:55:28 +0200

    finished third part

--- Aufgabe4/teil2-3/Woerterbuch.rb
+++ Aufgabe4/teil2-3/Woerterbuch.rb
@@ -1,3 +1,40 @@
 require "Set"
 class Woerterbuch 
+  include Enumerable
+
+  def initialize(sprachen)
+    @sprachen = sprachen
+    @words = Hash.new
+  end
+  
+  def [](wort)
+    return @words[wort]
+  end
+  
+  def []=(wort,uebersetzung)
+    if @words[wort] == nil then @words[wort] = Set.new end
+    @words[wort].add(uebersetzung)
+  end
+
+  def to_s()
+    str = "#{@sprachen}\n"
+    @words.each { |key,values|
+      str << "#{key}::"
+      values.each { |tmp|
+        str << "#{tmp}, "
+      }
+      str.chomp!(', ')
+      str << "\n"
+    }
+    return str
+  end
+
+  def each
+    if block_given?
+      @words.each { |key,value|
+        yield(key,value)
+      }
+    end
+  end
+  
 end
