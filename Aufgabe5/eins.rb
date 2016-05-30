18393beccf4a05d6d45b84493cdba4d572f74d6d
kontez <kontez@users.noreply.github.com>
2016-05-30 10:09:45 +0200

    bugfix

--- Aufgabe5/eins.rb
+++ Aufgabe5/eins.rb
@@ -9,23 +9,22 @@ def lni(x,n)
       log += ( (x-1.0)**(2.0*i+1.0) )/( (2.0*i+1.0)*(x+1.0)**(2.0*i+1.0) )
     end
   end
-  return log*2
+  return log
 end
 
 #ln rekursiv
 def lnr(x,n)
   if x <= 0 || n < 0 then raise ArgumentError end
-  if n==0 then return ((x-1.0)/(x+1.0))*2 end
+  if n==0 then return ((x-1.0)/(x+1.0)) end
   return lnr(x,n-1) + ( (x-1.0)**(2.0*n+1.0) )/( (2.0*n+1.0)*(x+1.0)**(2.0*n+1.0) )
 end
 
-#the result is supposed to be the same wtf?!
-puts lni(6,4)
-puts lnr(6,4)
+puts lni(6,4)*2
+puts lnr(6,4)*2
 
 #g iterativ
 def gi(n)
-  if n < 0 then raise ArgumentError end
+  if n <= 0 then raise ArgumentError end
   res = 0
   for i in (1..n)
     res += 1.0/(i*(i+1.0))
@@ -34,7 +33,7 @@ def gi(n)
 end
 
 def gr(n)
-  if n < 0 then raise ArgumentError end
+  if n <= 0 then raise ArgumentError end
   if n == 1 then return 0.5 end
   return gr(n-1) + 1.0/(n*(n+1.0))
 end
